f6a76d584208c50546913e91c5b6eb6b8bbe9624
Robert Anderson <rob@StarburstMac.local>
2021-04-22 03:29:42 +0200

    - Look ma, no VPN! We got rid of the VPN, which gets rid of a lot of   complexity and a separate thing that has to be managed in every   region. This also makes it more suitable for using in customer   environments, not just for Starburst demos! - Took out a lot of the kubectl waits, and added in a LOT more   home-built wait logic, that ensures that services are actually running   _and_ responding to requests before proceeding. This has made things   _much_ more reliable and predictable. - Everything, everything is now on a private subnet, on all three   clouds. No more public load balancers, api server endpoints, etc. - Added in ASCII art. Yes, we're back to the 1980s :-] <3

--- gcp/bigquery.tf
+++
@@ -1,7 +0,0 @@
-# In order for the service users associated with the GKE nodes to be allowed to
-# initiate reads, we have to grant them BigQuery 'user' privileges.
-resource "google_project_iam_member" "bqread" {
-  project    = data.google_project.project.project_id
-	role       = "roles/bigquery.user"
-  member     = "serviceAccount:${google_service_account.gke_servacct.email}"
-}
