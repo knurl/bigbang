10b81e2a52e6da7a39a97eda3b13035abee110cd
Rob Anderson <knurl@me.com>
2023-01-27 20:39:24 +0000

    - Update certificates. - In beta: Allow client installer to work on MacOS.

--- linux-install.sh
+++ linux-install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 function p {
     Color_Off='\033[0m'       # Text Reset
@@ -12,54 +12,90 @@ function ensure_in_profile {
     if ! grep -qF -- "$LINE" "$FILE"; then
 	echo Inserting "$LINE" into "$FILE"
 	echo "$LINE" >> "$FILE"
+    else
+	echo "$FILE" already has "$LINE"
     fi
 }
 
-# get rid of interactive menu in Ubuntu 22 when build-essential is installed
-sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" \
-    /etc/needrestart/needrestart.conf
+function is_ubuntu {
+    return 1
+}
+
+if $(is_ubuntu); then
+    HOMEBREWPATH="/home/linuxbrew/.linuxbrew"
+else
+    if [[ $(uname -m) == 'arm64' ]]; then
+	HOMEBREWPATH="/opt/homebrew"
+    else
+	HOMEBREWPATH="/usr/local"
+    fi
+fi
 
 p "cleaning up old python build logs and working trees"
 rm -rf /tmp/python*
 
-p "updating and upgrading packages with apt-get"
-sudo apt-get -y update
-sudo apt-get -y upgrade
+if $(is_ubuntu); then
+    # get rid of interactive menu in Ubuntu 22 when build-essential is installed
+    sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" \
+	/etc/needrestart/needrestart.conf
 
-p "installing base packages necessary for brew"
-sudo apt-get -y install build-essential libssl-dev zlib1g-dev libbz2-dev \
-    libreadline-dev libsqlite3-dev curl llvm libncursesw5-dev xz-utils \
-    tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-    libgdbm-compat-dev libgdbm-dev autoconf libtool git 
+    p "updating and upgrading packages with apt-get"
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
+
+    p "installing base packages necessary for brew"
+    sudo apt-get -y install build-essential libssl-dev zlib1g-dev libbz2-dev \
+	libreadline-dev libsqlite3-dev curl llvm libncursesw5-dev xz-utils \
+	tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+	libgdbm-compat-dev libgdbm-dev autoconf libtool git 
+fi
+
+echo $HOMEBREWPATH
 
 p "installing brew"
-NONINTERACTIVE=1 /bin/bash -c \
-    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-ensure_in_profile '# Set PATH, MANPATH, etc., for Homebrew.'
-ensure_in_profile 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew doctor
+if [ ! $(which brew) ]; then
+    CMD='/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    echo $CMD
+    if $(! is_ubuntu); then
+	eval "$CMD"
+    else
+	eval "NONINTERACTIVE=1 $CMD"
+    fi
+    ensure_in_profile '# Set PATH, MANPATH, etc., for Homebrew.'
+    CMD="eval \"$(${HOMEBREWPATH}/bin/brew shellenv)\""
+    ensure_in_profile "$CMD"
+    eval "$CMD"
+    brew doctor
+else
+    echo "Brew is already installed"
+fi
 
 p "installing dependencies for BigBang"
 brew install gcc awscli azure-cli aws-iam-authenticator helm kubectl libyaml \
     terraform gimme-aws-creds pyenv $OPENSSL jmeter
 
 p "getting trino JDBC jarfile"
-JMETERPREFIX=$(brew --prefix jmeter)
-JMETERLIB=$JMETERPREFIX/libexec/lib
 TRINOROOTVERSION=393
 TRINOUPDATE=e.7
 TRINOORIGVERSION=${TRINOROOTVERSION}e
 TRINOVERSION=$TRINOROOTVERSION-$TRINOUPDATE
 TRINOJDBC=trino-jdbc-$TRINOVERSION.jar
 TRINOJDBCURL=https://s3.us-east-2.amazonaws.com/software.starburstdata.net/$TRINOORIGVERSION/$TRINOVERSION/trino-jdbc-$TRINOVERSION.jar
-if wget $TRINOJDBCURL; then
-    mv $TRINOJDBC $JMETERLIB
-    rm -f ${TRINOJDBC}*
+JMETERPREFIX=$(brew --prefix jmeter)
+JMETERLIB=$JMETERPREFIX/libexec/lib
+JMETERLIBTGT="${JMETERLIB}/$TRINOJDBC"
+if [ ! -f $JMETERLIBTGT ]; then
+    brew install wget
+    if wget $TRINOJDBCURL; then
+	mv $TRINOJDBC $JMETERLIBTGT
+	rm -f ${TRINOJDBC}*
+    else
+	RC=$?
+	echo Failed to get $TRINOJDBC at $TRINOJDBCURL
+	exit $RC
+    fi
 else
-    RC=$?
-    echo Failed to get $TRINOJDBC at $TRINOJDBCURL
-    exit $RC
+    echo "Trino JDBC jarfile already in place"
 fi
 
 p "determining correct versions of dependencies"
@@ -74,12 +110,12 @@ echo "OPenSSL lib directory is $OPENSSLLIB"
 echo "OPenSSL include directory is $OPENSSLINCLUDE"
 
 p "setting up pyenv"
-ensure_in_profile 'export PYENV_ROOT="$HOME/.pyenv"'
-ensure_in_profile 'export PATH="$PYENV_ROOT/bin:$PATH"'
-ensure_in_profile 'eval "$(pyenv init -)"'
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+ensure_in_profile 'export PYENV_ROOT="$HOME/.pyenv"'
+ensure_in_profile 'export PATH="$PYENV_ROOT/bin:$PATH"'
+ensure_in_profile 'eval "$(pyenv init -)"'
 p "pyenv version is $(pyenv --version)"
 
 p "installing python ${PYVERSION} and setting as system default version"
@@ -102,16 +138,30 @@ p "installing python dependencies for BigBang"
 pip install --upgrade jinja2 pyyaml psutil requests tabulate termcolor
 
 p "installing BigBang"
-git clone https://github.com/knurl/bigbang
+if [ ! -d bigbang ]; then
+    git clone https://github.com/knurl/bigbang
+else
+    echo "Bigbang already installed"
+fi
 
 p "configuring AWS profile"
-aws configure
+if [[ ! -f $HOME/.aws/config || ! -f $HOME/.aws/credentials ]]; then
+    aws configure
+else
+    echo "AWS config and credentials files already exist"
+fi
 
 p "configuring Okta"
-gimme-aws-creds -c
+if [[ ! -f $HOME/.okta_aws_login_config ]]; then
+    gimme-aws-creds -c
+else
+    echo "OKTA config already exists"
+fi
 
-#pip install google-cloud-bigquery google-cloud-storage
-#brew install --cask google-cloud-sdk
-#az configure
-#az login
-#gcloud init
+if ! $(is_ubuntu); then
+    pip install google-cloud-bigquery google-cloud-storage
+    brew install --cask google-cloud-sdk
+    if [[ ! -d $HOME/.config/gcloud ]]; then
+	gcloud init
+    fi
+fi
