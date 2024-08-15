bbe2877708009b2c6348512745639230d65c80db
Rob Anderson <knurl@me.com>
2024-08-15 22:19:19 +0200

    More

--- setup.sh
+++ setup.sh
@@ -46,7 +46,15 @@ p "***BIGBANG INSTALLATION STARTING***"
 p "cleaning up old python build logs and working trees"
 rm -rf /tmp/python*
 
+PYVERSION=3.11
+echo "python version is $PYVERSION"
+
 if is_ubuntu ; then
+    #
+    ##########
+    # Ubuntu #
+    ##########
+    #
     p "Ubuntu: Installing necessary packages using apt-get and snap"
     # get rid of interactive menu in Ubuntu 22 when build-essential is installed
     sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" \
@@ -57,7 +65,7 @@ if is_ubuntu ; then
     sudo apt-get -y upgrade
 
     p "installing apt packages for bigbang"
-    sudo apt-get -y install azure-cli python3 python3-pip
+    sudo apt-get install -y azure-cli
 
     p "Installing snap packages for bigbang"
     sudo snap install aws-cli --classic
@@ -83,7 +91,19 @@ if is_ubuntu ; then
     ensure_in_file "source $GCLOUDDIR/completion.zsh.inc" ~/.zshrc
     ensure_in_file "source $GCLOUDDIR/path.zsh.inc" ~/.zshrc
     popd
+
+    p "installing Python $PYVERSION"
+    sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt-get install -y python3.11
+    sudo apt-get install -y python3.11-venv
+
 else
+
+    #
+    #########
+    # MacOS #
+    #########
+    #
     p "installing brew"
     if [ ! $(which brew) ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -94,7 +114,7 @@ else
     fi
 
     p "installing dependencies for bigbang"
-    brew install gcc awscli azure-cli kubectl helm terraform pyenv $OPENSSL
+    brew install gcc awscli azure-cli kubectl helm terraform $OPENSSL python@3.11
 
     OPENSSL="openssl@3"
 
@@ -104,40 +124,15 @@ else
     echo "OpenSSL path is $OPENSSLPATH"
     echo "OPenSSL lib directory is $OPENSSLLIB"
     echo "OPenSSL include directory is $OPENSSLINCLUDE"
-
-    p "determining correct versions of dependencies"
-    PYVERSION=3.11.5
-    echo "python version is $PYVERSION"
-
-    p "setting up pyenv"
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-    ensure_in_profile '# pyenv'
-    ensure_in_profile 'export PYENV_ROOT="$HOME/.pyenv"'
-    ensure_in_profile 'export PATH="$HOMEBREWPATH/bin:$PYENV_ROOT/bin:$PATH"'
-    ensure_in_profile 'eval "$(pyenv init -)"'
-    p "pyenv version is $(pyenv --version)"
-
-    p "installing python ${PYVERSION} and setting as system default version"
-    export LDFLAGS="-Wl,-rpath,$OPENSSLLIB -L$OPENSSLLIB"
-    export CPPFLAGS="-I$OPENSSLINCLUDE"
-    export SSH=$OPENSSLPATH
-    export CONFIGURE_OPTS="-with-openssl=$OPENSSLPATH"
-    echo LDFLAGS=$LDFLAGS
-    echo CPPFLAGS=$CPPFLAGS
-    echo SSH=$SSH
-    echo CONFIGURE_OPTS=$CONFIGURE_OPTS
-    pyenv install -s $PYVERSION
-    pyenv global $PYVERSION
-
-    p "installing pip"
-    $PYTHON -m ensurepip --upgrade
-    pip install --upgrade pip
 fi
 
+p "setting up Python $PYVERSION virtual environment"
+python3.11 -m venv .venv # automatically also installs pip
+source .venv/bin/activate
+
 p "installing python dependencies for bigbang"
-pip install --upgrade jinja2 pyyaml psutil requests tabulate termcolor mypy types-requests
+pip install --upgrade jinja2 pyyaml psutil requests tabulate \
+    termcolor mypy types-requests
 
 #
 # Configure AWS
@@ -167,7 +162,7 @@ fi
 # Configure GCP
 #
 pip install google-cloud-bigquery google-cloud-storage
-bash -l
+source ~/.bashrc
 gcloud components update
 gcloud components install gke-gcloud-auth-plugin --quiet
 if [[ ! -d $HOME/.config/gcloud ]]; then
