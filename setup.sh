06048556b6e94ef3e9e828f2383ddbdb2e0c6ecf
Rob Anderson <knurl@me.com>
2024-08-15 19:14:08 +0200

    Fix ubuntu test in setup.sh

--- setup.sh
+++ setup.sh
@@ -18,9 +18,10 @@ function ensure_in_profile {
 }
 
 unameOut=$(uname -s)
+echo This system is $unameOut
 
 function is_ubuntu {
-    echo UNAME=$unameOut
+    echo This system is $unameOut
     case "$unameOut" in
 	Linux*)	return 0;;
 	Darwin*) return 1;;
@@ -28,9 +29,7 @@ function is_ubuntu {
     esac
 }
 
-if is_ubuntu; then
-    HOMEBREWPATH="/home/linuxbrew/.linuxbrew"
-else
+if ! is_ubuntu ; then
     if [[ $(uname -m) == 'arm64' ]]; then
 	HOMEBREWPATH="/opt/homebrew"
     else
@@ -43,106 +42,105 @@ p "***BIGBANG INSTALLATION STARTING***"
 p "cleaning up old python build logs and working trees"
 rm -rf /tmp/python*
 
-if is_ubuntu; then
+if is_ubuntu ; then
+    p "Ubuntu: Installing necessary packages using apt-get and snap"
     # get rid of interactive menu in Ubuntu 22 when build-essential is installed
     sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" \
-	/etc/needrestart/needrestart.conf
+        /etc/needrestart/needrestart.conf
 
     p "updating and upgrading packages with apt-get"
     sudo apt-get -y update
     sudo apt-get -y upgrade
 
-    p "installing base packages necessary for brew"
-    sudo apt-get -y install build-essential libssl-dev zlib1g-dev libbz2-dev \
-	libreadline-dev libsqlite3-dev curl llvm libncursesw5-dev xz-utils \
-	tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-	libgdbm-compat-dev libgdbm-dev autoconf libtool git 
-fi
+    p "installing apt packages for bigbang"
+    sudo apt-get -y install azure-cli python3 python3-pip
 
-echo $HOMEBREWPATH
+    p "Installing snap packages for bigbang"
+    sudo snap install aws-cli --classic
+    sudo snap install kubectl --classic
+    sudo snap install helm --classic
+    sudo snap install terraform --classic
 
-p "installing brew"
-if [ ! $(which brew) ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    p "Installing apt packages for gcloud"
+    sudo apt-get install -y apt-transport-https ca-certificates gnupg curl
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    sudo apt-get install -y google-cloud-cli
 else
-    echo "Brew is already installed"
+    p "installing brew"
+    if [ ! $(which brew) ]; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Brew is already installed"
+        export PATH="$HOMEBREWPATH/bin:$PATH"
+        echo $HOMEBREWPATH
+    fi
+
+    p "installing dependencies for bigbang"
+    brew install gcc awscli azure-cli kubectl helm terraform pyenv $OPENSSL
+
+    OPENSSL="openssl@3"
+
+    OPENSSLPATH=$(readlink -f $(brew --prefix $OPENSSL))
+    OPENSSLLIB=$(readlink -f $OPENSSLPATH/lib)
+    OPENSSLINCLUDE=$(readlink -f $OPENSSLPATH/include)
+    echo "OpenSSL path is $OPENSSLPATH"
+    echo "OPenSSL lib directory is $OPENSSLLIB"
+    echo "OPenSSL include directory is $OPENSSLINCLUDE"
+
+    p "determining correct versions of dependencies"
+    PYVERSION=3.11.5
+    echo "python version is $PYVERSION"
+
+    p "setting up pyenv"
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    ensure_in_profile '# pyenv'
+    ensure_in_profile 'export PYENV_ROOT="$HOME/.pyenv"'
+    ensure_in_profile 'export PATH="$HOMEBREWPATH/bin:$PYENV_ROOT/bin:$PATH"'
+    ensure_in_profile 'eval "$(pyenv init -)"'
+    p "pyenv version is $(pyenv --version)"
+
+    p "installing python ${PYVERSION} and setting as system default version"
+    export LDFLAGS="-Wl,-rpath,$OPENSSLLIB -L$OPENSSLLIB"
+    export CPPFLAGS="-I$OPENSSLINCLUDE"
+    export SSH=$OPENSSLPATH
+    export CONFIGURE_OPTS="-with-openssl=$OPENSSLPATH"
+    echo LDFLAGS=$LDFLAGS
+    echo CPPFLAGS=$CPPFLAGS
+    echo SSH=$SSH
+    echo CONFIGURE_OPTS=$CONFIGURE_OPTS
+    pyenv install -s $PYVERSION
+    pyenv global $PYVERSION
+
+    p "installing pip"
+    $PYTHON -m ensurepip --upgrade
+    pip install --upgrade pip
 fi
-
-export PATH="$HOMEBREWPATH/bin:$PATH"
-
-OPENSSL="openssl@3"
-
-p "installing dependencies for bigbang"
-brew install gcc awscli azure-cli kubectl helm terraform pyenv $OPENSSL
-
-OPENSSLPATH=$(readlink -f $(brew --prefix $OPENSSL))
-OPENSSLLIB=$(readlink -f $OPENSSLPATH/lib)
-OPENSSLINCLUDE=$(readlink -f $OPENSSLPATH/include)
-echo "OpenSSL path is $OPENSSLPATH"
-echo "OPenSSL lib directory is $OPENSSLLIB"
-echo "OPenSSL include directory is $OPENSSLINCLUDE"
-
-p "determining correct versions of dependencies"
-PYVERSION=3.11.5
-echo "python version is $PYVERSION"
-
-p "setting up pyenv"
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-ensure_in_profile '# pyenv'
-ensure_in_profile 'export PYENV_ROOT="$HOME/.pyenv"'
-ensure_in_profile 'export PATH="$HOMEBREWPATH/bin:$PYENV_ROOT/bin:$PATH"'
-ensure_in_profile 'eval "$(pyenv init -)"'
-p "pyenv version is $(pyenv --version)"
-
-p "installing python ${PYVERSION} and setting as system default version"
-export LDFLAGS="-Wl,-rpath,$OPENSSLLIB -L$OPENSSLLIB"
-export CPPFLAGS="-I$OPENSSLINCLUDE"
-export SSH=$OPENSSLPATH
-export CONFIGURE_OPTS="-with-openssl=$OPENSSLPATH"
-echo LDFLAGS=$LDFLAGS
-echo CPPFLAGS=$CPPFLAGS
-echo SSH=$SSH
-echo CONFIGURE_OPTS=$CONFIGURE_OPTS
-pyenv install -s $PYVERSION
-pyenv global $PYVERSION
-
-p "installing pip"
-python -m ensurepip --upgrade
-pip install --upgrade pip
 
 p "installing python dependencies for bigbang"
-pip install --upgrade jinja2 pyyaml psutil requests tabulate termcolor \
-    mypy types-requests
-
-#p "installing bigbang"
-#if [ ! -d bigbang ]; then
-#    git clone https://github.com/knurl/bigbang ~/bigbang
-#else
-#    echo "Bigbang already installed"
-#fi
+pip install --upgrade jinja2 pyyaml psutil requests tabulate termcolor mypy types-requests
 
 # Configure AWS
 #
-p "configuring AWS profile"
-if [[ ! -f $HOME/.aws/config ]]; then
-    echo Configuring SSO login. When prompted below...
-    echo --> SSO Start URL as https://hazelcast.awsapps.com/start
-    echo --> When prompted below, specify SSO Region as us-east-1
-    aws configure sso
-else
-    echo "AWS config file already exists"
-fi
+#p "configuring AWS profile"
+#if [[ ! -f $HOME/.aws/config ]]; then
+#    echo Configuring SSO login. When prompted below...
+#    echo --> SSO Start URL as https://hazelcast.awsapps.com/start
+#    echo --> When prompted below, specify SSO Region as us-east-1
+#    aws configure sso
+#else
+#    echo "AWS config file already exists"
+#fi
 
 # Configure Azure
 #
-p "configuring Azure profile"
-az configure
-az login
+#p "configuring Azure profile"
+#az configure
+#az login
 
 pip install google-cloud-bigquery google-cloud-storage
-brew install --cask google-cloud-sdk
 gcloud components install gke-gcloud-auth-plugin
 if [[ ! -d $HOME/.config/gcloud ]]; then
     gcloud init
@@ -151,7 +149,6 @@ else
 fi
 
 gcloud components update
-gcloud auth login
-gcloud auth application-default login
 
 p "***BIGBANG INSTALLATION COMPLETE***"
+
